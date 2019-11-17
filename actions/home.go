d2859c68d22a8d70a6c4c39fa7861038b6138594
ec0341ks <with0526@icloud.com>
2019-11-17 18:10:47 +0900

    コードの修正

--- actions/home.go
+++ actions/home.go
@@ -5,10 +5,10 @@ import "github.com/gobuffalo/buffalo"
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
+	c.Set("name","yui")
 	return c.Render(200, r.HTML("index.html"))
 }
 
 func YuiHandler(c buffalo.Context) error {
-	c.Set("name","yui")
 	return c.Render(200, r.HTML("yui.html"))
 }

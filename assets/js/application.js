618243ab8d10c23aa23dfa5040cccac2b9249972
ec0341ks <idc68730@gmail.com>
2019-11-20 18:53:00 +0900

    jqueryのテスト動作

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,9 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
 
-$(() => {
 
-});
+
+$('.profile').on('click', function () {
+  $(this).remove();
+  console.log("発火！")
+})

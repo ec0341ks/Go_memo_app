960f17e74690dd5de32ca97459fadca13e55d2eb
ec0341ks <idc68730@gmail.com>
2019-11-25 20:07:53 +0900

    jqueryの導入

--- assets/js/application.js
+++ assets/js/application.js
@@ -3,7 +3,20 @@ require("bootstrap/dist/js/bootstrap.bundle.js");
 
 
 
-// $('.delete-button').on('click', function () {
-//   $(this).parent().remove();
-//   console.log("発火！")
-// })
+$('.delete-button').on('click', function (e) {
+  e.preventDefault();
+  var nameid = $(this).closest(".profile").children(".id").text();
+  var checked = confirm("本当に削除しますか？")
+  if (checked == true) {
+    $(this).closest(".profile").remove();
+    $.ajax({
+      type: 'DELETE',
+      url: '/boards/' + nameid,
+      dataType: 'json'
+    })
+    alert("削除しました")
+  }
+  else {
+    alert("削除されませんでした")
+  }
+})
